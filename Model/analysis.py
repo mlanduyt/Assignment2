1f013f5447530711a6b551db10d7964d43db10ca
mlanduyt <mlanduyt@bu.edu>
2025-02-24 15:10:30 +0000

    readme

--- Model/analysis.py
+++ Model/analysis.py
@@ -25,29 +25,12 @@ class Elements:
         self.elem_load = np.zeros((12,self.numElements))
         self.L = np.zeros(self.numElements)+1  
 
-
-def solver(Nodes,Elements):
-    
-    K_global = find_global_frame_stiffness(Nodes,Elements)
-    partition_K_global, partition_forces, num_unknown_disp = partition_matrices(Nodes,Elements,K_global)
-
-    known_forces = partition_forces[0:num_unknown_disp]
-    K_known_forces = partition_K_global[0:num_unknown_disp,0:num_unknown_disp]
-
-    Delta_f = np.linalg.solve(K_known_forces,known_forces)
-    K_unknown_forces = partition_K_global[0:num_unknown_disp,num_unknown_disp:]
-    F_reaction = K_unknown_forces @ Delta_f
-    return Delta_f, F_reaction
-
-
 def find_global_frame_stiffness(Nodes,Elements):
     K_global=np.zeros((Nodes.numNodes*6,Nodes.numNodes*6))
     for elm_ind in range(Elements.numElements):
 
-        # find element end points
         node0 = Elements.connections[elm_ind][0]
         node1 = Elements.connections[elm_ind][1]
-
         x1 = Nodes.nodes[node0][0]
         y1 = Nodes.nodes[node0][1]
         z1 = Nodes.nodes[node0][2]
@@ -55,10 +38,10 @@ def find_global_frame_stiffness(Nodes,Elements):
         y2 = Nodes.nodes[node1][1]
         z2 = Nodes.nodes[node1][2]
 
-        # element length
+        # length of element
         Elements.L[elm_ind] = math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2 )
 
-        # DOF indices (add one for indexing)
+        # DOF indices
         node1_dof_ind1 = 6 * node0
         node1_dof_ind2 = 6 * (node0 + 1)
         node2_dof_ind1 = 6 * node1
@@ -97,7 +80,18 @@ def partition_matrices(Nodes,Elements,K_global):
 
     partition_forces = np.vstack((Nodes.forces[unknown_disp],Nodes.forces[known_disp]))
     partition_K_global = np.vstack((K_global[unknown_disp],K_global[known_disp]))
-
     num_unknown_disp = np.size(unknown_disp)
-
     return partition_K_global, partition_forces, num_unknown_disp
+
+def solver(Nodes,Elements):
+    
+    K_global = find_global_frame_stiffness(Nodes,Elements)
+    partition_K_global, partition_forces, num_unknown_disp = partition_matrices(Nodes,Elements,K_global)
+
+    known_forces = partition_forces[0:num_unknown_disp]
+    K_known_forces = partition_K_global[0:num_unknown_disp,0:num_unknown_disp]
+
+    Delta_f = np.linalg.solve(K_known_forces,known_forces)
+    K_unknown_forces = partition_K_global[0:num_unknown_disp,num_unknown_disp:]
+    F_reaction = K_unknown_forces @ Delta_f
+    return Delta_f, F_reaction
--- README.md
+++ README.md
@@ -17,7 +17,8 @@ python --version
 ```
 install the applicable pips utilized for this code
 ```bash
-pip install sympy
 pip install numpy
 pip install pathlib
 ```
+
+This code is meant to create and solve structural matrices, these can be defined by creating node points which are then connected via 'connections'. The inputs for this analysis can be found in the tutorial.ipynb section of this file. These numbers may be changed and evaluated by the solver which calls the main portion of the analysis from analysis.py. Some functions are utilized from the math_utils.py script.
